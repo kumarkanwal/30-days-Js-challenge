d7844545e81c12869004ee6d7a72ddea483fdb33
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-07 17:13:37 +0500

    first commit

---
+++ 30-days/day1/main.js
@@ -0,0 +1,34 @@
+
+    // 1. declaration let x;
+    // 2. assignment x = 100;
+
+
+    // variable =  A container that stoes a CSSMathValue. 
+                // Bahaves as if it were the value it conains
+    //  data types  
+
+    // numbers  
+    let age = 25;
+    //    let price = 10.99;
+    //    let gpa = 2.1; 
+
+    // string  
+    let firstName = "kanwal";
+    //  let favouriteFood = "pizza";
+    //  let email = "kanwal123@gmail.com"; 
+
+    // Boolean  
+    // let online = true; 
+    // let forSale = false; 
+    let isstudent = true;
+
+    
+
+
+    // document.getElementById("p1").textContent = firstName;
+    // document.getElementById("p2").textContent = age;
+    // document.getElementById("p3").textContent = isstudent;
+
+    document.getElementById("p1").textContent = `your name is ${firstName}`;
+    document.getElementById("p2").textContent = `your age is ${age}`;
+    document.getElementById("p3").textContent =  `Enrolled student ${isstudent}`;
