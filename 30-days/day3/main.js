1de2e17fc24eaa6c0eb2c6e5ae0b136d16ed3149
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-09 13:32:35 +0500

    MATHS OBJECTS

---
+++ 30-days/day3/main.js
@@ -0,0 +1,22 @@
+/* Math =  bilt in object that provides a collection of properties and methods  */
+
+let x = 3;
+let y = 2;
+let z;
+
+z = Math.round(x); // round to the nearest integer
+z = Math.floor(x); // round to down
+z = Math.ceil(x); // round to upper value
+z = Math.trunc(x); // just removes decimal value
+z = Math.pow(z, y);  // power on the number
+z = Math.sqrt(x); // square root function
+z = Math.log(x); // maths log value
+z = Math.sin(x); // math tignomantory sin function
+z = Math.cos(x); // math tignomantory cos function
+z = Math.tan(x); // math tignomantory tan function
+z = Math.abs(x); // eliminat the negative sign
+z = Math.sign(z); // - value = -1 , 0 value = 0 , positive value = 1
+let max = Math.max(x,y,z); // finds the maximum value
+let min = Math.min(x,y,z); // finds the minimum value
+
+
