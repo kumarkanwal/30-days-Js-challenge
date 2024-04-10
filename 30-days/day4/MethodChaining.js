24adaf6c2de7c3a8f5f8f47ad3fe38fcd947690d
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-10 14:24:56 +0500

    For loops

--- 30-days/day4/MethodChaining.js
+++ 30-days/day4/MethodChaining.js
@@ -22,3 +22,21 @@ console.log(username);
 // ------------ METHOD CHAIN --------------------------------
 
 username= username.trim().charAt(0).toLocaleUpperCase()  + username.trim().slice(1).toLowerCase()
+
+
+
+//logical opeartors
+
+// logical operators  = used to combine or manipulate boolean values (true or false) 
+// AND = &&  // need to satisfy all the conditions to trues
+// OR = ||  // need to satisfy one of the condition ture
+// NOT = ! //  not equal too 
+
+
+
+
+//  = assignment operator;
+// == comparison operator ( compare if values are equal);
+// === strict equality operatoe( compare if values & datatype are equal);
+// != inequality operator 
+// !== strict equality operatoe
---
+++ 30-days/day4/whileLoops.js
@@ -0,0 +1,41 @@
+// let username = "";
+// while (username === "" || username == null){
+//     username = window.prompt("Enter your username");
+//     console.log(username);
+// }
+// do{
+//     username = window.prompt("Enter your username");
+//     console.log(username);
+// }while (username === "" || username == null)
+
+
+
+// let loggedIn = false;
+// let username;
+// let password;
+
+// while(!loggedIn){
+//     username = window.prompt("Enter your username");
+//     password = window.prompt("Enter your password");
+
+//     if(username === "myUsername" && password === "myPassword"){
+//         loggedIn = true;
+//         console.log("you are logged in!");
+//     }else{
+//         console.log("Invalid credentials! please try again");
+//     }
+// }
+
+
+// for loop = repeat some code a LIMTED amount of times
+
+// for(let i = 0; i < 20  ; i++){
+
+//     if(i == 13){
+//         continue;
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+
