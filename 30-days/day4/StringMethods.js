0215cedf353ab0dfe0b7a9fbbca31552d88c10ac
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-10 11:54:53 +0500

    String slicing

--- 30-days/day4/StringMethods.js
+++ 30-days/day4/StringMethods.js
@@ -2,10 +2,10 @@
 
 let userName = "kanwalKumar";
 
-console.log(userName.charAt(1)); // find the character at the particular index
-console.log(userName.indexOf("w")); // find the index of the particular character , no march = -1
+//console.log(userName.charAt(1)); // find the character at the particular index
+//console.log(userName.indexOf("w")); // find the index of the particular character , no march = -1
 userName.lastIndexOf("w") // find the index of the character starting from last
-userName.length() // geives the length of the string 
+userName.length // geives the length of the string 
 userName.trim() // trim the white space before or after the string.
 userName.toUpperCase() // Make all string to uppercase
 userName.toLowerCase() // Turn whole string to Lowercase
@@ -18,4 +18,24 @@ userName.padStart(15) // make the string to particular length by adding extra ch
 userName.padEnd(20) // make the string to particular length by adding extra character at the end of the string.
 
 
+
+// string slicing and creating a sub string from a portion of another string
+                              
+                                // String.slice(startindex, endIndex)
+
+let fullName = "kanwal Kumar";
+
+// let firstName = fullName.slice(0,6)
+// let lastName = fullName.slice(7 ); // if want to slice till end so no need to provide end index
+
+
+let firstName = fullName.slice(0, fullName.indexOf(" "))
+let lastName = fullName.slice(fullName.indexOf(" ") + 1)
+console.log(firstName)
+console.log(lastName)
+
+
+
+
+                                
 
