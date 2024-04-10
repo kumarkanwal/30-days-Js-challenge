3e762163513d61523d8c21f5bc7e70e132a88843
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-10 10:51:41 +0500

    String methods in javascript

---
+++ 30-days/day4/StringMethods.js
@@ -0,0 +1,21 @@
+// string methods = allow you to manupulate and work with text (strings)
+
+let userName = "kanwalKumar";
+
+console.log(userName.charAt(1)); // find the character at the particular index
+console.log(userName.indexOf("w")); // find the index of the particular character , no march = -1
+userName.lastIndexOf("w") // find the index of the character starting from last
+userName.length() // geives the length of the string 
+userName.trim() // trim the white space before or after the string.
+userName.toUpperCase() // Make all string to uppercase
+userName.toLowerCase() // Turn whole string to Lowercase
+userName.repeat() // Repeat the string number of times as stated
+userName.startsWith() // check if the string start with the particular character / boolean answer
+userName.endsWith() // check if the string ends with the particular character / boolean answer
+userName.includes() // check if string includes particular character
+userName.replaceAll() // replace the particular character with any other character
+userName.padStart(15) // make the string to particular length by adding extra character at the start.
+userName.padEnd(20) // make the string to particular length by adding extra character at the end of the string.
+
+
+
