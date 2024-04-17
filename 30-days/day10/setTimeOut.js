d0679dbd85ecf155afff8bdc8b442220167709bd
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-17 12:12:59 +0500

    day10

--- 30-days/day10/setTimeOut.js
+++ 30-days/day10/setTimeOut.js
@@ -3,8 +3,27 @@
 // Times are approximation (varies based on the workload of the Javscript runtime env.)
 // setTimeout(callback,delay); 
 
-function sayHello(){
-    window.alert("hello buddy");
-}
+// function sayHello(){
+//     window.alert("hello buddy");
+// }
 
-setTimeout(sayHello, 3000);
+// setTimeout(sayHello, 3000);
+// setTimeout(() => window.alert("hello kanwal is here"), 3000);
+
+
+// const timeoutId = setTimeout(() => window.alert("Hello"),3000);
+
+// to cancle setTimeout function 
+// clearTimeout(timeoutId);
+
+
+let timeoutId;
+
+
+function startTimer(){
+   timeoutId = setTimeout(() => window.alert("Hello"),3000);
+};
+
+function clearTimer(){
+        clearTimeout(timeoutId);    
+}
