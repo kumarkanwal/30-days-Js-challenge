10609291807daacc3d246eeaef2a32dca9c4a957
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-21 10:01:51 +0500

    Day 13

--- 30-days/day12/addAndChange.js
+++ 30-days/day12/addAndChange.js
@@ -17,15 +17,32 @@ newH1.textContent = "I like burger";
 
 // document.body.append(newH1);
 // document.body.prepend(newH1);
-document.getElementById("box1").append(newH1);
+// document.getElementById("box1").append(newH1);
 
 // document.body.insertBefore(newElement,currentElement);
-document.body.insertBefore(newH1,box3);
+// document.body.insertBefore(newH1,box3);
 // REMOVE HTML ELEMENT
 
 // document.getElementById('box1').removeChild(newH1)
-document.body.removeChild(newH1)
+// document.body.removeChild(newH1)
 
+
+
+
+const newListItem = document.createElement("li");
+
+newListItem.textContent = "cononut";
+newListItem.id = 'coconut';
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = 'lightgreen';
+
+document.getElementById("banana")
+document.getElementById('fruits').insertBefore(newListItem,banana);
+
+// if fruit items(li) dont have any id 
+
+const frutis = document.querySelectorAll("#fruits li");
+document.getElementById('fruits').insertBefore(,)
 
 
 
---
+++ day13/eventlistener.js
@@ -0,0 +1,5 @@
+// eventListener = Listen for specific event to create interactive web pages
+// events: click, mouseover, mouseout
+// .addEventListener(event,callback);
+
+
